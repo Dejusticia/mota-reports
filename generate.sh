2cadf0ee9c2c6cf4cc6314893456e15f23452dee
Celso Bessa <celso@celsobessa.com.br>
2020-05-18 07:01:16 -0500

    recreated new reports stubs

--- generate.sh
+++ generate.sh
@@ -5,9 +5,11 @@ while read line; do
     # reading each line
     RNUMBER=$(($RANDOM%9))
     cp report-default-0$RNUMBER.json report-www-$line.json
+    cp report-default-0$RNUMBER.json report-$line.json
     # echo "Line No. $n : $line"
     # sed 's/www.ejemplo.gov.co/$line/g' report-default-07.json
     sed -i "" -e "s/www.ejemplo.gov.co/$line/g" report-$line.json
+    sed -i "" -e "s/www.ejemplo.gov.co/$line/g" report-www-$line.json
     n=$((n+1))
     echo "created file number" $n
     echo "with the name" $line
