f9872ba3847b89e9d94c867a17370fb1e5cf9003
Celso Bessa <celso@celsobessa.com.br>
2020-05-18 06:33:01 -0500

    pre generate new reports

--- generate.sh
+++ generate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 filename='active-domains.txt'
 n=1
-epoch=1580216400
 while read line; do
     # reading each line
-    cp report-default.json $line
-    echo "Line No. $n : $line"
+    RNUMBER=$(($RANDOM%Y))
+    cp report-default-0$RNUMBER.json report-$line.json
+    # echo "Line No. $n : $line"
+    # sed 's/www.ejemplo.gov.co/$line/g' report-default-07.json
+    sed -i "" -e 's/www.ejemplo.gov.co/$line/g' report-$line.json
     n=$((n+1))
 done < $filename
