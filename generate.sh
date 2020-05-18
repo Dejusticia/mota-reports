db972ef991b338a6cf694ada36880cfd13f71f23
Celso Bessa <celso@celsobessa.com.br>
2020-05-18 06:50:41 -0500

    generated new stub reports with www;

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ n=1
 while read line; do
     # reading each line
     RNUMBER=$(($RANDOM%9))
-    cp report-default-0$RNUMBER.json report-$line.json
+    cp report-default-0$RNUMBER.json report-www-$line.json
     # echo "Line No. $n : $line"
     # sed 's/www.ejemplo.gov.co/$line/g' report-default-07.json
     sed -i "" -e "s/www.ejemplo.gov.co/$line/g" report-$line.json
